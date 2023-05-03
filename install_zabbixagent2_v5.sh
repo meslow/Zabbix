f3a00d17ddbb007df90554f1c0a267622b7efd0f
Meslow76 <meslow@gmail.com>
2023-05-03 18:15:44 +0200

    Update install_zabbixagent2_v5.sh

--- install_zabbixagent2_v5.sh
+++ install_zabbixagent2_v5.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Pour executer penser a faire un chmod +x install_zabbixagent2_v5.sh
 # Vérifier si l'utilisateur a les privilèges d'administration
 if [ "$EUID" -ne 0 ]; then
   echo "Ce script doit être exécuté en tant qu'administrateur."
@@ -8,8 +7,7 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 # Mettre à jour le système
-# apt update && apt upgrade -y
-apt update -y
+apt update && apt upgrade -y
 
 # Installer les dépendances
 apt install -y wget
@@ -20,14 +18,32 @@ dpkg -i zabbix-release_5.0-1+focal_all.deb
 apt update
 apt install -y zabbix-agent
 
+# Demander à l'utilisateur de saisir l'adresse IP du serveur Zabbix
+read -p "Veuillez saisir l'adresse IP du serveur Zabbix : " IP_DU_SERVEUR_ZABBIX
+
+# Demander à l'utilisateur de saisir le nom de la machine
+read -p "Veuillez saisir le nom de la machine : " Nom_DE_LA_MACHINE
+
+# Demander à l'utilisateur si les lignes TLS doivent être configurées
+read -p "Voulez-vous configurer les lignes TLS ? (Oui/Non) : " CONFIGURER_TLS
+
 # Configurer l'agent Zabbix
-sed -i 's/Server=127.0.0.1/Server=IP_DU_SERVEUR_ZABBIX/' /etc/zabbix/zabbix_agentd.conf
-sed -i 's/ServerActive=127.0.0.1/ServerActive=IP_DU_SERVEUR_ZABBIX/' /etc/zabbix/zabbix_agentd.conf
-sed -i 's/Hostname=Zabbix server/Hostname=Nom_DE_LA_MACHINE/' /etc/zabbix/zabbix_agentd.conf
-sed -i 's/# TLSAccept=unencrypted/TLSAccept=psk/' /etc/zabbix/zabbix_agentd.conf
-sed -i 's/# TLSConnect=unencrypted/TLSConnect=psk/' /etc/zabbix/zabbix_agentd.conf
-sed -i 's/# TLSPSKIdentity=NOM_IDENTITE' /etc/zabbix/zabbix_agentd.conf
-sed -i 's/# TLSPSKFile=/TLSPSKFile=/etc/zabbix/pskfile.psk' /etc/zabbix/zabbix_agentd.conf
+sed -i "s/Server=127.0.0.1/Server=$IP_DU_SERVEUR_ZABBIX/" /etc/zabbix/zabbix_agentd.conf
+sed -i "s/ServerActive=127.0.0.1/ServerActive=$IP_DU_SERVEUR_ZABBIX/" /etc/zabbix/zabbix_agentd.conf
+sed -i "s/Hostname=Zabbix server/Hostname=$Nom_DE_LA_MACHINE/" /etc/zabbix/zabbix_agentd.conf
+
+# Configurer les lignes TLS si demandé
+if [[ "$CONFIGURER_TLS" =~ ^[Oo][Uu][Ii]$ ]]; then
+  sed -i 's/# TLSAccept=unencrypted/TLSAccept=psk/' /etc/zabbix/zabbix_agentd.conf
+  sed -i 's/# TLSConnect=unencrypted/TLSConnect=psk/' /etc/zabbix/zabbix_agentd.conf
+  read -p "Veuillez saisir l'identité TLSPSK : " TLSPSKIdentity
+  read -p "Veuillez saisir le chemin vers le fichier TLSPSK : " TLSPSKFile
+  echo "TLSPSKIdentity=$TLSPSKIdentity"
+  echo "TLSPSKFile=$TLSPSKFile"
+  sed -i 's/# TLSPSKIdentity=/TLSPSKIdentity=$TLSPSKIdentity/' /etc/zabbix/zabbix_agentd.conf
+  sed -i 's/# TLSPSKFile=/TLSPSKFile=$TLSPSKFile/' /etc/zabbix/zabbix_agentd.conf
+  echo "Configuration PSK effectuée"
+fi
 
 # Démarrer l'agent Zabbix
 systemctl start zabbix-agent
